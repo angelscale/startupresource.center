451d1726d7bd08eac22ea8f162702a9e1e6dd4ec
Yasin Junet <yasintze@gmail.com>
2021-11-12 06:39:16 +0700

    feat: add service section

---
+++ src/views/home/service-section.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Main } from 'templates/PortfolioGrid/components';
+
+import { services } from 'templates/PortfolioGrid/data';
+
+const ServiceSectionView = () => {
+  return (
+    <>
+      <Main data={services} />
+    </>
+  );
+};
+
+export default ServiceSectionView;
