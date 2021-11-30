7c9a5724d9553d9e7dcba7eed8e1e3218071435e
Yasin Junet <yasintze@gmail.com>
2021-11-30 11:18:28 +0700

    fix: resolve feedback on the homepage design

--- src/views/home/service-section.js
+++ src/views/home/service-section.js
@@ -2,16 +2,10 @@ import React from 'react';
 
 import { Main } from 'templates/PortfolioGrid/components';
 import { services } from 'templates/PortfolioGrid/data';
-import SectionLabel from 'components/SectionLabel';
 
 const ServiceSectionView = () => {
   return (
     <>
-      <SectionLabel
-        align="center"
-        title="Our Services"
-        subtitle="Do pariatur velit eu incididunt pariatur non ullamco exercitation eu mollit veniam sint eiusmod."
-      />
       <Main data={services} />
     </>
   );
