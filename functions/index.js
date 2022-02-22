f6430caa872dace1c6c9ce8a7f71bde0dac1d374
Nathan Cerny <ncerny@angel-scale.com>
2022-02-22 09:25:11 -0600

    chore: Add firebase configuration.  Bump node-version to 16.x
    Signed-off-by: Nathan Cerny <ncerny@angel-scale.com>

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
