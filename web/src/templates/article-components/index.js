0368f5e01832dd041d996956947eecc582ec659a
MadCoderr <msrfarooqi@gmail.com>
2022-03-31 12:25:21 +0500

    remove unused code

--- web/src/templates/article-components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Content } from './content.component';
-export { default as Hero } from './hero.component';
-export { default as SidebarArticles } from './sidebar-articles.component';
-export { default as SidebarNewsletter } from './sidebar-newsletter.component';
-export { default as SimilarStories } from './similar-stories.component';
