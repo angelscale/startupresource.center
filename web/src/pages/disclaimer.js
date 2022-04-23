ac04fe990ef46d0c684c604caa09e9314a4fc34c
Nathan Cerny <ncerny@gmail.com>
2022-04-23 09:23:44 -0500

    fix(termly): [CU-27qcc0c] Add TermlyEmbed Component.  Add pages for each policy. (#69)
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

---
+++ web/src/pages/disclaimer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TermlyEmbed } from 'components';
+
+const Disclaimer = () => (
+  <TermlyEmbed dataId="bdf46b7f-28ee-4bfe-b164-57cb439ebf37" />
+);
+
+export default Disclaimer;
--- web/src/pages/privacy-policy.js
+++ web/src/pages/privacy-policy.js
@@ -1,26 +1,8 @@
 import React from 'react';
+import { TermlyEmbed } from 'components';
 
-const termlyData = (d, s, id) => {
-  var js,
-    tjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = 'https://app.termly.io/embed-policy.min.js';
-  tjs.parentNode.insertBefore(js, tjs);
-};
-
-const PrivacyPolicy = () => {
-  if (typeof document !== 'undefined') {
-    termlyData(document, 'script', 'termly-jssdk');
-    return (
-      <div
-        name="termly-embed"
-        data-id="7b91ebc0-d1c0-4340-843a-f71ad086b064"
-        data-type="iframe"
-      />
-    );
-  } else return null;
-};
+const PrivacyPolicy = () => (
+  <TermlyEmbed dataId="7b91ebc0-d1c0-4340-843a-f71ad086b064" />
+);
 
 export default PrivacyPolicy;
