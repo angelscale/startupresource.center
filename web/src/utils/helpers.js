2380334a953f547fd86901434ba420683ac133ac
MadCoderr <msrfarooqi@gmail.com>
2022-05-12 21:56:05 +0500

    fix(ProductTemplate): [CU-29ganp2] Add spacing between bullet list and following header

--- web/src/utils/helpers.js
+++ web/src/utils/helpers.js
@@ -6,3 +6,17 @@ export const getFormattedDate = (date) => {
 
   return toDate.format('MMMM DD, YYYY');
 };
+
+export const extractProductContent = (_content) => {
+  const content = {};
+  const props = _content.props.children;
+
+  content.description = props[0].props.children[1];
+  content.features = props[4].props.children;
+  content.unique = props[8].props.children;
+  content.bestFor = props[12].props.children;
+  content.pricing = props[16].props.children;
+  content.info = props[20]?.props?.children;
+
+  return content;
+};
